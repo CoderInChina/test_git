13bae784a1b1068831c26e6e3a8d814cf8832ded
wc <wc@qq.com>
2019-11-09 08:52:36 +0800

    李四新分支加了东西了

--- git-test/src/com/shop/git/TestDemo1.java
+++ git-test/src/com/shop/git/TestDemo1.java
@@ -8,20 +8,16 @@ public class TestDemo1 {
         System.out.println("hello git");
 
 
-
     }
 
-    public void test()
-    {
+    public void test() {
         System.out.println("第二次修改了");
 
         System.out.printf("1111111111111111111111111111111111");
     }
 
-
-
-
-
-
-
+    public void test_brank()
+    {
+        System.out.printf("这个是 test-brank 过来 。。。。。。");
+    }
 }
